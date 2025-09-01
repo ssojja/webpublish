826fa23d4e166fe4392271dcc2f7bc3a9a84b0c3
kim <ssojja0987@gmail.com>
2025-09-01 17:43:25 +0900

    9.1 study commit

--- JavaScript/7.built-in/1_wrapper.js
+++ JavaScript/7.built-in/1_wrapper.js
@@ -3,14 +3,26 @@
  * 기본 데이터 타입에 클래스 형식으로 정의한 데이터 타입을 wrapper 클래스라고 함
  */
 
-// Number 클래스 형식
+//Number 클래스 형식
 let a = "123";
-if(typeof a === "string"){ parseInt(a);}
+if(typeof a === "string") { parseInt(a);}
 console.log(typeof a, a);
+if(a == 123) { console.log(typeof a, a);}
 
-if(a == 123){ console.log(typeof a, a); }
+const nobj1 = new Number(123);
+const nobj2 = new Number("123");
 
-// String : 문자열 클래스
+console.log(nobj1 instanceof Number, nobj1);
+console.log(nobj2 instanceof Number, nobj2);
+console.log(nobj1 == nobj2);  //주소값 비교
+console.log(nobj1.valueOf() == nobj2.valueOf()); //값 비교
+
+let maxValue = Number.MAX_VALUE;
+let minValue = Number.MIN_VALUE;
+console.log(maxValue, minValue);
+
+//String : 문자열 클래스
+console.clear();
 let str1 = new String("Hello");
 let str2 = new String("Hello");
 
@@ -19,4 +31,10 @@ console.log(str2, str2.length, str2.valueOf());
 console.log(str1 == str2);
 console.log(str1.valueOf() == str2.valueOf());
 
-// Boolean : 불리언 클래스
+//Boolean : 불리언 클래스
+console.clear();
+let flag1 = true;
+let flag2 = new Boolean(true);
+
+console.log(flag1 == flag2);
+console.log(flag1 == flag2.valueOf());
